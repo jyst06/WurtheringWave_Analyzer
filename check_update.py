59acfea8aadbefd2794b2df63971754ebe471af9
JamesYu <107468548+jyst06@users.noreply.github.com>
2024-06-06 19:07:50 +0800

    Delete check_update.py

--- check_update.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_latest_release():
-    response = requests.get("https://api.github.com/repos/jyst06/WurtheringWave_Analyzer/releases")
-    latest_release = response.json()
-    print(latest_release)
-
-if __name__ == '__main__':
-    get_latest_release()
