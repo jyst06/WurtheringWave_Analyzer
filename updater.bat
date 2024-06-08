de805de4cec1b068b02880c04f83bfdff1e6755e
JamesYu <107468548+jyst06@users.noreply.github.com>
2024-06-08 11:25:51 +0800

    Update updater.bat
    修改多餘內容

--- updater.bat
+++ updater.bat
@@ -1,10 +1,10 @@
 @echo off
 net session >nul 2>&1
-if %errorLevel% == 0 (
-    start "" "ui.exe"
-) else (
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+if not %errorLevel% == 0 (
+    echo Set UAC = CreateObject("Shell.Application") > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "updater.exe", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
+) else (
+    start "" "updater.exe"
 )
